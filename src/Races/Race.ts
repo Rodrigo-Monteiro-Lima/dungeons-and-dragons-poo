df25d64f7d41b4f61196c4a1224417e107a44861
Rodrigo <rodrigoml2395@gmail.com>
2023-03-24 14:33:33 -0300

    feat: Race class

---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race {
+  #name: string;
+  #dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this.#name = name;
+    this.#dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this.#name;
+  }
+
+  get dexterity(): number {
+    return this.#dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
